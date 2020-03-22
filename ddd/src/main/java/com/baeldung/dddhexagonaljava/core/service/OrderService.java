3ad61e41fd20d30efb8c0b71eec7a0e81cf8804b
gindex <yevgen.pikus@gmail.com>
2020-03-22 15:01:52 +0100

    Add example of hexagonal architecture in java

---
+++ ddd/src/main/java/com/baeldung/dddhexagonaljava/core/service/OrderService.java
@@ -0,0 +1,25 @@
+package com.baeldung.dddhexagonaljava.core.service;
+
+import com.baeldung.dddhexagonaljava.core.domain.Order;
+import com.baeldung.dddhexagonaljava.ports.NotificationService;
+
+import java.text.MessageFormat;
+
+public class OrderService {
+
+    private NotificationService notificationService;
+
+    public OrderService(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
+    public void createOrder(Order order) {
+        //create order
+
+        String message = MessageFormat.format("Created new order with id `{0}` for `{1}` items.", order.getId(),
+          order.getProductOrders().size());
+
+        notificationService.sendNotification(order.getCustomer(), message);
+    }
+
+}
